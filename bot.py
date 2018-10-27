98ea043a336c44c5405d5b517b8d350c547c52fb
eienEika <dmatro86@gmail.com>
2018-10-27 20:37:11 +0300

    added some echos

--- bot.py
+++ bot.py
@@ -1,13 +1,24 @@
 import telebot
-import config
+import secret
+from config import *
+
+bot = telebot.TeleBot(secret.token)
 
 
-bot = telebot.TeleBot(config.token)
-
-
-@bot.message_handler(commands='start')
+@bot.message_handler(commands=['start'])
 def start(message):
-	bot.send_message(message.chat.id, "Я пожилой бот")
+	bot.send_message(message.chat.id, "Я пожилой бот", reply_markup=bt_start)
+
+
+@bot.message_handler()
+def command(message):
+	bot.send_message(message.chat.id, "Шо?")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+	if call.data == 'courses':
+		bot.send_message(call.message.chat.id, 'dadaya')
 
 
 bot.polling()
