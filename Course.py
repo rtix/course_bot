576c1490f5402b77b39872c3edcb3aa392489124
rtix <radif.tazetdinov@mail.ru>
2018-12-03 09:51:32 +0300

    Course.py merged

--- Course.py
+++ Course.py
@@ -17,17 +17,8 @@ class Literature():
 		self.__course_id = course_id
 		if number:
 			self.__lit_number = number
-			data_from_base = CourseDB.get_literature(self.__course_id, self.__lit_number)
-			self.__name = data_from_base["name"]
-			self.__description = data_from_base["description"]
-			self.__file_id = data_from_base["file_id"]
-			self.__url = data_from_base["url"]
 		else:
-			self.__name = name
-			self.__description = description
-			self.__file_id = file_id
-			self.__url = url
-			self.__lit_number = CourseDB.create_literature(self.__course_id, self.__name, self.__description, self.__file_id, self.__url)
+			self.__lit_number = CourseDB.create_literature(self.__course_id, name, description, file_id, url)
 	@property
 	def course_id(self):
 		return self.__course_id
@@ -36,47 +27,40 @@ class Literature():
 		return self.__lit_number
 	@property
 	def name(self):
-		return self.__name
+		return CourseDB.get_literature(self.__course_id, self.__lit_number)["name"]
 	@name.setter
 	def name(self, name):
-		self.__name = name
-		CourseDB.set_literature(self.__course_id, self.__lit_number, 'name', self.__name)
+		CourseDB.set_literature(self.__course_id, self.__lit_number, 'name', name)
 	@property
 	def description(self):
-		return self.__description
+		return CourseDB.get_literature(self.__course_id, self.__lit_number)["description"]
 	@description.setter
 	def description(self, description):
-		self.__description = description
-		CourseDB.set_literature(self.__course_id, self.__lit_number, 'description', self.__description)
+		CourseDB.set_literature(self.__course_id, self.__lit_number, 'description', description)
 	@property
 	def file_id(self):
-		return self.__file_id
+		return CourseDB.get_literature(self.__course_id, self.__lit_number)["file_id"]
 	@file_id.setter
 	def file_id(self, file_id):
-		self.__file_id = file_id
-		CourseDB.set_literature(self.__course_id, self.__lit_number, 'file_id', self.__file_id)
+		CourseDB.set_literature(self.__course_id, self.__lit_number, 'file_id', file_id)
 	@property
 	def url(self):
-		return self.__url
+		return CourseDB.get_literature(self.__course_id, self.__lit_number)["url"]
 	@url.setter
 	def url(self, url):
 		self.__url = url
-		CourseDB.set_literature(self.__course_id, self.__lit_number, 'url', self.__url)
+		CourseDB.set_literature(self.__course_id, self.__lit_number, 'url', url)
 	def delete(self):
 		CourseDB.delete_literature(self.__course_id, self.__lit_number)
-		del self.__course_id, self.__lit_number, self.__name, self.__description, self.__file_id, self.__url
+		del self.__course_id, self.__lit_number
 
 class Attendance():
 	def __init__(self, course_id, cw_number, user_id):
 		self.__course_id = course_id
 		self.__cw_number = cw_number
 		self.__user_id = user_id
-		data_from_base = CourseDB.get_attendance(self.__course_id, self.__cw_number, self.__user_id)
-		if data_from_base:
-			self.__value = data_from_base["value"]
-		else:
-			self.__value = False
-			CourseDB.create_attendance(self.__course_id, self.__cw_number, self.__user_id, self.__value)
+		if not(CourseDB.get_attendance(self.__course_id, self.__cw_number, self.__user_id)):
+			CourseDB.create_attendance(self.__course_id, self.__cw_number, self.__user_id, False)
 	@property
 	def course_id(self):
 		return self.__course_id
@@ -88,14 +72,13 @@ class Attendance():
 		return self.__user_id
 	@property
 	def value(self):
-		return self.__value
+		return CourseDB.get_attendance(self.__course_id, self.__cw_number, self.__user_id)["value"]
 	@value.setter
 	def value(self, value):
-		self.__value = value
-		CourseDB.set_attendance(self.__course_id, self.__cw_number, self.__user_id, 'value', self.__value)
+		CourseDB.set_attendance(self.__course_id, self.__cw_number, self.__user_id, 'value', value)
 	def delete(self):
 		CourseDB.delete_attendance(self.__course_id, self.__cw_number, self.__user_id)
-		del self.__course_id, self.__cw_number, self.__user_id, self.__value
+		del self.__course_id, self.__cw_number, self.__user_id
 
 class Classwork():
 	def __init__(self, course_id, number=None, name="", date=None):
@@ -105,13 +88,8 @@ class Classwork():
 		self.__course_id = course_id
 		if number:
 			self.__cw_number = number
-			data_from_base = CourseDB.get_classwork(self.__course_id, self.__cw_number)
-			self.__cw_name = data_from_base["name"]
-			self.__cw_date = data_from_base["date"]
 		else:
-			self.__cw_name = name
-			self.__cw_date = date
-			self.__cw_number = CourseDB.create_classwork(self.__course_id, self.__cw_name, self.__cw_date)
+			self.__cw_number = CourseDB.create_classwork(self.__course_id, name, date)
 	@property
 	def course_id(self):
 		return self.__course_id
@@ -120,37 +98,29 @@ class Classwork():
 		return self.__cw_number
 	@property
 	def name(self):
-		return self.__cw_name
+		return CourseDB.get_classwork(self.__course_id, self.__cw_number)["name"]
 	@name.setter
 	def name(self, name):
-		self.__cw_name = name
-		CourseDB.set_classwork(self.__course_id, self.__cw_number, 'name', self.__cw_name)
+		CourseDB.set_classwork(self.__course_id, self.__cw_number, 'name', name)
 	@property
 	def date(self):
-		return self.__cw_date
+		return CourseDB.get_classwork(self.__course_id, self.__cw_number)["date"]
 	@date.setter
 	def date(self, date):
-		self.__cw_date = date
-		CourseDB.set_classwork(self.__course_id, self.__cw_number, 'date', self.__cw_date)
+		CourseDB.set_classwork(self.__course_id, self.__cw_number, 'date', date)
 	def attendance(self, user_id):
 		return Attendance(self.__course_id, self.__cw_number, user_id)
 	def delete(self):
 		CourseDB.delete_classwork(self.__course_id, self.__cw_number)
-		del self.__course_id, self.__cw_number, self.__cw_name, self.__cw_date
+		del self.__course_id, self.__cw_number
 
 class Mark():
 	def __init__(self, course_id, task_number, user_id):
 		self.__course_id = course_id
 		self.__task_number = task_number
 		self.__user_id = user_id
-		data_from_base = CourseDB.get_mark(self.__course_id, self.__task_number, self.__user_id)
-		if data_from_base:
-			self.__mrk = data_from_base["mark"]
-			self.__mrk_date = data_from_base["date"]
-		else:
-			self.__mrk = None
-			self.__mrk_date = None
-			CourseDB.create_mark(self.__course_id, self.__task_number, self.__user_id, self.__mrk, self.__mrk_date)
+		if not(CourseDB.get_mark(self.__course_id, self.__task_number, self.__user_id)):
+			CourseDB.create_mark(self.__course_id, self.__task_number, self.__user_id, None, None)
 	@property
 	def course_id(self):
 		return self.__course_id
@@ -162,24 +132,21 @@ class Mark():
 		return self.__user_id
 	@property
 	def value(self):
-		return self.__mrk
+		return CourseDB.get_mark(self.__course_id, self.__task_number, self.__user_id)["mark"]
 	@value.setter
 	def value(self, mrk):
-		self.__mrk = mrk
-		if not self.__mrk_date:
-			self.__mrk_date = date.today()
-		CourseDB.set_mark(self.__course_id, self.__task_number, self.__user_id, 'mark', self.__mrk)
-		CourseDB.set_mark(self.__course_id, self.__task_number, self.__user_id, 'date', self.__mrk_date)
+		if not(CourseDB.get_mark(self.__course_id, self.__task_number, self.__user_id)["date"]):
+			CourseDB.set_mark(self.__course_id, self.__task_number, self.__user_id, 'date', date.today())
+		CourseDB.set_mark(self.__course_id, self.__task_number, self.__user_id, 'mark', mrk)
 	@property
 	def date(self):
-		return self.__mrk_date
+		return CourseDB.get_mark(self.__course_id, self.__task_number, self.__user_id)["date"]
 	@date.setter
 	def date(self, date):
-		self.__mrk_date = date
-		CourseDB.set_mark(self.__course_id, self.__task_number, self.__user_id, 'date', self.__mrk_date)
+		CourseDB.set_mark(self.__course_id, self.__task_number, self.__user_id, 'date', date)
 	def delete(self):
 		CourseDB.delete_mark(self.__course_id, self.__task_number, self.__user_id)
-		del self.__course_id, self.__task_number, self.__user_id, self.__mrk
+		del self.__course_id, self.__task_number, self.__user_id
 
 class Task():
 	def __init__(self, course_id, number=None, name="", description="", highest_mark=None):
@@ -189,55 +156,43 @@ class Task():
 		self.__course_id = course_id
 		if number:
 			self.__task_number = number
-			data_from_base = CourseDB.get_task(self.__course_id, self.__task_number)
-			self.__task_name = data_from_base["name"]
-			self.__task_description = data_from_base["description"]
-			self.__task_highest_mark = data_from_base["highest_mark"]
-			self.__deadline = data_from_base["deadline"]
 		else:
-			self.__task_name = name
-			self.__task_description = description
-			self.__task_highest_mark = highest_mark
-			self.__deadline = None
-			self.__task_number = CourseDB.create_task(self.__course_id, self.__task_name, self.__task_description, self.__task_highest_mark, self.__deadline)
+			self.__task_number = CourseDB.create_task(self.__course_id, name, description, highest_mark, None)
 	@property
 	def course_id(self):
 		return self.__course_id
 	@property
 	def name(self):
-		return self.__task_name
+		return CourseDB.get_task(self.__course_id, self.__task_number)["name"]
 	@name.setter
 	def name(self, name):
-		self.__task_name = name
-		CourseDB.set_task(self.__course_id, self.__task_number, 'name', self.__task_name)
+		CourseDB.set_task(self.__course_id, self.__task_number, 'name', name)
 	@property
 	def number(self):
 		return self.__task_number
 	@property
 	def description(self):
-		return self.__task_description
+		return CourseDB.get_task(self.__course_id, self.__task_number)["description"]
 	@description.setter
 	def description(self, description):
-		self.__task_description = description
-		CourseDB.set_task(self.__course_id, self.__task_number, 'description', self.__description)
+		CourseDB.set_task(self.__course_id, self.__task_number, 'description', description)
 	@property
 	def highest_mark(self):
-		return self.__task_highest_mark
+		return CourseDB.get_task(self.__course_id, self.__task_number)["highest_mark"]
 	@highest_mark.setter
 	def highest_mark(self, highest_mark):
-		self.__task_highest_mark = highest_mark
-		CourseDB.set_task(self.__course_id, self.__task_number, 'highest_mark', self.__task_highest_mark)
+		CourseDB.set_task(self.__course_id, self.__task_number, 'highest_mark', highest_mark)
 	@property
 	def deadline(self):
-		return self.__deadline
+		return CourseDB.get_task(self.__course_id, self.__task_number)["deadline"]
 	@deadline.setter
-	def deadline(self):
-		CourseDB.set_task(self.__course_id, self.__task_number, 'deadline', self.__deadline)
+	def deadline(self, deadline):
+		CourseDB.set_task(self.__course_id, self.__task_number, 'deadline', deadline)
 	def mark(self, user_id):
 		return Mark(self.__course_id, self.__task_number, user_id)
 	def delete(self):
 		CourseDB.delete_task(self.__course_id, self.__task_number)
-		del self.__course_id, self.__task_number, self.__task_name, self.__task_description, self.__task_highest_mark
+		del self.__course_id, self.__task_number
 
 class Course():
 	def __init__(self, course_id=None, owner_id=None, name=""):
@@ -246,82 +201,55 @@ class Course():
 			raise InterrelatedParametersError(("course_id"), ("owner_id", "name"))
 		if course_id:
 			self.__course_id = course_id
-			data_from_base = CourseDB.get_course(self.__course_id)
-			self.__course_name = data_from_base["name"]
-			self.__owner_id = data_from_base["owner"]
-			self.__description = data_from_base["description"]
-			self.__participants = data_from_base["participants"]
-			self.__blacklist = data_from_base["blacklist"]
-			self.__task_numbers = data_from_base["task_numbers"]
-			self.__cw_numbers = data_from_base["cw_numbers"]
-			self.__lit_numbers = data_from_base["lit_numbers"]
-			self.__entry_restriction = data_from_base["entry_restriction"]
 		else:
-			self.__owner_id = owner_id
-			self.__course_name = name
-			self.__course_id = CourseDB.create_course(self.__owner_id, self.__course_name)
-			self.__description = ""
-			self.__participants = []
-			self.__blacklist = []
-			self.__task_numbers = []
-			self.__cw_numbers = []
-			self.__lit_numbers = []
-			self.__entry_restriction = None
+			self.__course_id = CourseDB.create_course(owner_id, name)
 	@property
 	def id(self):
 		return self.__course_id
 	@property
 	def name(self):
-		return self.__course_name
+		return CourseDB.get_course(self.__course_id)["name"]
 	@name.setter
 	def name(self, name):
-		self.__course_name = name
-		CourseDB.set_course(self.__course_id, 'name', self.__course_name)
+		CourseDB.set_course(self.__course_id, 'name', name)
 	@property
 	def owner(self):
-		return User.User(self.__owner_id)
+		return User.User(CourseDB.get_course(self.__course_id)["owner"])
 	@property
 	def description(self):
-		return self.__description
+		return CourseDB.get_course(self.__course_id)["description"]
 	@description.setter
 	def description(self, description):
-		self.__description = description
-		CourseDB.set_course(self.__course_id, 'description', self.__description)
+		CourseDB.set_course(self.__course_id, 'description', description)
 	@property
 	def entry_restriction(self):
-		return self.__entry_restriction
+		return CourseDB.get_course(self.__course_id)["entry_restriction"]
 	@entry_restriction.setter
 	def entry_restriction(self, restriction_date):
-		self.__entry_restriction = restriction_date
-		CourseDB.set_course(self.__course_id, "entry_restriction", self.__entry_restriction)
+		CourseDB.set_course(self.__course_id, "entry_restriction", restriction_date)
 	@property
 	def participants(self):
-		return tuple(User.User(user_id) for user_id in self.__participants)
+		return tuple(User.User(user_id) for user_id in CourseDB.get_course(self.__course_id)["participants"])
 	@property
 	def blacklist(self):
-		return tuple(User.User(user_id) for user_id in self.__blacklist)
+		return tuple(User.User(user_id) for user_id in CourseDB.get_course(self.__course_id)["blacklist"])
 	@property
 	def tasks(self):
-		return tuple(Task(self.__course_id, task_number) for task_number in self.__task_numbers)
+		return tuple(Task(self.__course_id, task_number) for task_number in CourseDB.get_course(self.__course_id)["task_numbers"])
 	@property
 	def classworks(self):
-		return tuple(Classwork(self.__course_id, cw_number) for cw_number in self.__cw_numbers)
+		return tuple(Classwork(self.__course_id, cw_number) for cw_number in CourseDB.get_course(self.__course_id)["cw_numbers"])
 	@property
 	def literatures(self):
-		return tuple(Literature(self.__course_id, lit_number) for lit_number in self.__lit_numbers)
+		return tuple(Literature(self.__course_id, lit_number) for lit_number in CourseDB.get_course(self.__course_id)["lit_numbers"])
 	def append_student(self, user_id):
-		self.__participants.append(user_id)
 		CourseDB.append_user_course(user_id, self.__course_id)
 	def remove_student(self, user_id):
-		self.__participants.remove(user_id)
 		CourseDB.remove_user_course(user_id, self.__course_id)
 	def append_to_blacklist(self, user_id):
-		self.__blacklist.append(user_id)
 		CourseDB.append_to_blacklist(self.__course_id, user_id)
-		self.__participants.remove(user_id)
 		CourseDB.remove_user_course(user_id, self.__course_id)
 	def remove_from_blacklist(self, user_id):
-		self.__blacklist.remove(user_id)
 		CourseDB.remove_from_blacklist(self.__course_id, user_id)
 	def task(self, task_number=None, task_name="", task_description="", task_highest_mark=None):
 		return Task(self.__course_id, task_number, task_name, task_description, task_highest_mark)
@@ -330,9 +258,12 @@ class Course():
 	def literature(self, lit_number=None, name="", description="", file_id=None, url=""):
 		return Literature(self.__course_id, lit_number, name, description, file_id, url)
 	def mailing(self, email, password, text):
-		participants = [User.User(user_id) for user_id in self.__participants]
+		participants = [User.User(user_id) for user_id in CourseDB.get_course(self.__course_id)["participants"]]
 		recipients = [participant.email for participant in participants]
 		Mail(email, password).send(recipients, text)
 	def delete(self):
 		CourseDB.delete_course(self.__course_id)
-		del self.__course_id, self.__course_name, self.__owner_id, self.__description
+		del self.__course_id
+
+def fetch_all_courses():
+	return tuple(Course(course_id) for course_id in CourseDB.fetch_all_courses())
